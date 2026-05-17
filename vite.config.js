b4fdcd371b528a016be16b1084abc0b810ebc262
dr-abdullah-24 <abdullahfaisalljmu@gmail.com>
2026-05-17 01:09:11 +0100

    Set custom domain to ems.decomrobotics.com

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/decom-robotics-employees-managment-system/',
+  base: '/',
   build: {
     rollupOptions: {
       output: {
